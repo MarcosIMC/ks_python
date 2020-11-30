334a73d0388358147a34a1325f50a2a0ca3ee2ef
Jonathan Gómez <joni.gf91@gmail.com>
2020-11-30 21:25:01 +0000

    added "final" return methods and ejecution time

--- Bag.py
+++ Bag.py
@@ -1,18 +1,26 @@
 from io import open
+from typing import List, Any
+from time import time
 from QuickSort import quicksort
-class Bag:
-    n_elements = 0
-    freeSpace = 0
-    elementsValue = 0
-    listValue = []
-    listWeight =[]
 
+
+class Bag:
+
+    n_elements: int = 0
+    freeSpace: int = 0
+    elementsValue: int = 0
+    listValue: list = []
+    listWeight: list = []
+    origin_list: list = []
+    dt: list = []
+    ttime = 0
 
     def __init__(self, route):
+        start_time = time()
         file = open(route, "r")
         arr = file.readlines()
         file.close()
-        for i in range (len(arr)):
+        for i in range(len(arr)):
             if i == 0:
                 vec = arr[i].split()
                 self.n_elements = int(vec[0])
@@ -21,11 +29,38 @@ class Bag:
                 vec = arr[i].split()
                 self.listValue.append(int(vec[0]))
                 self.listWeight.append(int(vec[1]))
-        quicksort(self.listValue,0,self.n_elements -1)
+                self.origin_list.append(int(vec[0]))
+        quicksort(self.listValue, 0, self.n_elements - 1)
         quicksort(self.listWeight, 0, self.n_elements - 1)
+        self.get_by_max_value()
+        self.ttime = time() - start_time
 
-    def getValue(self):
+    def get_value(self):
         return self.listValue
-    def getWeight(self):
+
+    def get_weight(self):
         return self.listWeight
 
+    def value_bag(self):
+        return self.elementsValue
+
+    def get_space(self):
+        return self.freeSpace
+
+    def get_dt(self):
+        return self.dt
+
+    def get_time(self):
+        return self.ttime
+
+    def get_by_max_value(self):
+        for i in range(self.n_elements):
+            if self.freeSpace >= self.listWeight[i]:
+                self.freeSpace -= self.listWeight[i]
+                self.elementsValue += self.listValue[i]
+                for j in range(self.n_elements):
+                    if self.listValue[i] == self.origin_list[j]:
+                        self.dt.append(j)
+                        break
+            if self.freeSpace == 0:
+                break
--- main.py
+++ main.py
@@ -1,19 +1,9 @@
-import Option_H
+
 import sys
-import os
 from Bag import Bag
-from random import randint
 
-def load_file(ruta):
-    print("Cargando el fichero")
-    file = open(ruta, "r")
-    arr = eval(file.read())
-    for i in arr:
-        print(i)
-    file.close()
-    #quicksort(arr, 0, len(arr)-1)
 
-def load_directory(ruta):
+"""def load_directory(ruta):
     print("Cargando todos los ficheros de la ruta")
     list = os.listdir(ruta)
     number_file = 1
@@ -25,46 +15,30 @@ def load_directory(ruta):
         arr = eval(data)
         data.close()
         quicksort(arr, 0, len(arr)-1)
-        number_file = number_file+1
-
-def quicksort(arr, start, end):
-    if start < end:
-        index = partition(arr, start, end)
-        quicksort(arr, start, index-1)
-        quicksort(arr, index+1, end)
-
-def partition(arr, start, end):
-    pivot = randint(start, end) #Escogemos un valor aleatorio entre los rangos
-    last_element = arr[end]
-    arr[end] = arr[pivot]
-    arr[pivot] = last_element
-
-    index = start
-
-    for i in range(start, end):
-        if arr[i] <= arr[end]:
-            aux = arr[i]
-            arr[i] = arr[index]
-            arr[index] = aux
-            index += 1
-        aux_1 = arr[end]
-        arr[end] = arr[index]
-        arr[index] = aux_1
-
-        return index
+        number_file = number_file+1"""
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    if(len(sys.argv) > 1 and len(sys.argv) < 7):
+    """if(len(sys.argv) > 1 and len(sys.argv) < 7):
         for i in range(0, len(sys.argv)):
             if sys.argv[1] == "-f" and i == 1:
                 print(sys.argv[i])
-                mochila = Bag(sys.argv[2])
+                mochas = Bag(sys.argv[2])
     else:
         Option_H
 
     print(mochila.getValue())
-    print(mochila.getWeight())
+    print(mochila.getWeight())"""
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    for i in range(0, len(sys.argv)):
+        if sys.argv[1] == "-f" and i == 1:
+            print(sys.argv[i])
+            mochila = Bag(sys.argv[2])
+print("lista ordenada valores", mochila.get_value())
+print("lista ordenada pesos", mochila.get_weight())
+print("valor obtenido", mochila.value_bag())
+print("espacio final", mochila.get_space())
+print("indices", mochila.get_dt())
+print("lista original valores", mochila.origin_list)
+print("tiempo en segundos ", mochila.get_time())
